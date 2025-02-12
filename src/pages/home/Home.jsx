0c05b6aa49808a60708832c556987b61225697f4
nigarr111 <nigarra-sp102@code.edu.az>
2025-02-12 19:45:10 +0400

    responsiv

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,13 +2,11 @@ import React from 'react'
 import Header from '../../components/header/Header'
 import NewFilms from '../../components/newFilms/NewFilms'
 import Films from '../../components/Films/Films'
-import Search from '../../components/search/Search'
 
 const Home = () => {
   return (
     <div>
       <Header/>
-      <Search/>
       <NewFilms/>
       <Films/>
     </div>

b1313e637b9761e7155f9f696d37c4e07b2d256a
Nigar Ahmadli <nigarra-sp102@code.edu.az>
2025-02-23 23:41:39 +0400

    Delete Backend directory

--- Backend/routes/commentRoute.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { addComment } from '../controllers/commentController.js';
-
-const router = express.Router();
-
-router.post('/movies/:movieId/comments', addComment);
-
-export default router;
